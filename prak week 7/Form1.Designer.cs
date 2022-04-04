a2029036919f98eb522caf7c21781d0d2fd48352
angelineivana <angelineivana01@student.ciputra.ac.id>
2022-04-04 19:55:28 +0700

    Add project files.

---
+++ prak week 7/Form1.Designer.cs
@@ -0,0 +1,156 @@
+﻿
+namespace prak_week_7
+{
+    partial class FormPandaQuiz
+    {
+        /// <summary>
+        ///  Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        ///  Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        ///  Required method for Designer support - do not modify
+        ///  the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.labelKalimat = new System.Windows.Forms.Label();
+            this.labelHuruf = new System.Windows.Forms.Label();
+            this.textBoxInputKal = new System.Windows.Forms.TextBox();
+            this.textBoxInputHur = new System.Windows.Forms.TextBox();
+            this.labelMenjadi = new System.Windows.Forms.Label();
+            this.textBoxInputHur2 = new System.Windows.Forms.TextBox();
+            this.buttonKonfirm = new System.Windows.Forms.Button();
+            this.labelHasil = new System.Windows.Forms.Label();
+            this.labelOutput = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // labelKalimat
+            // 
+            this.labelKalimat.AutoSize = true;
+            this.labelKalimat.Location = new System.Drawing.Point(29, 35);
+            this.labelKalimat.Name = "labelKalimat";
+            this.labelKalimat.Size = new System.Drawing.Size(155, 25);
+            this.labelKalimat.TabIndex = 0;
+            this.labelKalimat.Text = "Masukkan Kalimat";
+            // 
+            // labelHuruf
+            // 
+            this.labelHuruf.AutoSize = true;
+            this.labelHuruf.Location = new System.Drawing.Point(29, 84);
+            this.labelHuruf.Name = "labelHuruf";
+            this.labelHuruf.Size = new System.Drawing.Size(142, 25);
+            this.labelHuruf.TabIndex = 1;
+            this.labelHuruf.Text = "Masukkan Huruf";
+            // 
+            // textBoxInputKal
+            // 
+            this.textBoxInputKal.Location = new System.Drawing.Point(254, 32);
+            this.textBoxInputKal.Name = "textBoxInputKal";
+            this.textBoxInputKal.Size = new System.Drawing.Size(488, 31);
+            this.textBoxInputKal.TabIndex = 2;
+            // 
+            // textBoxInputHur
+            // 
+            this.textBoxInputHur.Location = new System.Drawing.Point(254, 79);
+            this.textBoxInputHur.Name = "textBoxInputHur";
+            this.textBoxInputHur.Size = new System.Drawing.Size(140, 31);
+            this.textBoxInputHur.TabIndex = 3;
+            this.textBoxInputHur.TextChanged += new System.EventHandler(this.textBoxInputHur_TextChanged);
+            // 
+            // labelMenjadi
+            // 
+            this.labelMenjadi.AutoSize = true;
+            this.labelMenjadi.Location = new System.Drawing.Point(460, 82);
+            this.labelMenjadi.Name = "labelMenjadi";
+            this.labelMenjadi.Size = new System.Drawing.Size(75, 25);
+            this.labelMenjadi.TabIndex = 4;
+            this.labelMenjadi.Text = "Menjadi";
+            // 
+            // textBoxInputHur2
+            // 
+            this.textBoxInputHur2.Location = new System.Drawing.Point(575, 79);
+            this.textBoxInputHur2.Name = "textBoxInputHur2";
+            this.textBoxInputHur2.Size = new System.Drawing.Size(167, 31);
+            this.textBoxInputHur2.TabIndex = 5;
+            this.textBoxInputHur2.TextChanged += new System.EventHandler(this.textBoxInputHur2_TextChanged);
+            // 
+            // buttonKonfirm
+            // 
+            this.buttonKonfirm.Location = new System.Drawing.Point(254, 145);
+            this.buttonKonfirm.Name = "buttonKonfirm";
+            this.buttonKonfirm.Size = new System.Drawing.Size(488, 34);
+            this.buttonKonfirm.TabIndex = 6;
+            this.buttonKonfirm.Text = "Konfirmasi";
+            this.buttonKonfirm.UseVisualStyleBackColor = true;
+            this.buttonKonfirm.Click += new System.EventHandler(this.buttonKonfirm_Click);
+            // 
+            // labelHasil
+            // 
+            this.labelHasil.AutoSize = true;
+            this.labelHasil.Location = new System.Drawing.Point(29, 230);
+            this.labelHasil.Name = "labelHasil";
+            this.labelHasil.Size = new System.Drawing.Size(50, 25);
+            this.labelHasil.TabIndex = 7;
+            this.labelHasil.Text = "Hasil";
+            // 
+            // labelOutput
+            // 
+            this.labelOutput.AutoSize = true;
+            this.labelOutput.Location = new System.Drawing.Point(254, 230);
+            this.labelOutput.Name = "labelOutput";
+            this.labelOutput.Size = new System.Drawing.Size(24, 25);
+            this.labelOutput.TabIndex = 8;
+            this.labelOutput.Text = "...";
+            // 
+            // FormPandaQuiz
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(10F, 25F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(819, 301);
+            this.Controls.Add(this.labelOutput);
+            this.Controls.Add(this.labelHasil);
+            this.Controls.Add(this.buttonKonfirm);
+            this.Controls.Add(this.textBoxInputHur2);
+            this.Controls.Add(this.labelMenjadi);
+            this.Controls.Add(this.textBoxInputHur);
+            this.Controls.Add(this.textBoxInputKal);
+            this.Controls.Add(this.labelHuruf);
+            this.Controls.Add(this.labelKalimat);
+            this.Name = "FormPandaQuiz";
+            this.Text = "Form Panda Quiz";
+            this.Load += new System.EventHandler(this.FormPandaQuiz_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label labelKalimat;
+        private System.Windows.Forms.Label labelHuruf;
+        private System.Windows.Forms.TextBox textBoxInputKal;
+        private System.Windows.Forms.TextBox textBoxInputHur;
+        private System.Windows.Forms.Label labelMenjadi;
+        private System.Windows.Forms.TextBox textBoxInputHur2;
+        private System.Windows.Forms.Button buttonKonfirm;
+        private System.Windows.Forms.Label labelHasil;
+        private System.Windows.Forms.Label labelOutput;
+    }
+}
+
---
+++ prak week 7/Form1.cs
@@ -0,0 +1,165 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace prak_week_7
+{
+    public partial class FormPandaQuiz : Form
+    {
+        public FormPandaQuiz()
+        {
+            InitializeComponent();
+        }
+        public static List<char> alpha = new List<char>();
+        public static int urutan = 0;
+        public static int[] urutanHur = new int[2];
+        public static int[] urutanHur2 = new int[2];
+        public static int cekHur = 0;
+        public static int cekHur2 = 0;
+        private void FormPandaQuiz_Load(object sender, EventArgs e)
+        {
+            for (int i = 0; i < 2; i++)
+            {
+                for (char a = 'A'; a <= 'Z'; a++)
+                {
+                    alpha.Add(a);
+                    urutan++;
+                }
+            }
+        }
+       
+        private void textBoxInputHur_TextChanged(object sender, EventArgs e)
+        {
+            if (textBoxInputKal.Text.ToUpper().Contains(textBoxInputHur.Text.ToUpper()))
+            {
+                for (int i = 0; i < 2; i++)
+                {
+                    for (char a = 'A'; a <= 'Z'; a++)
+                    {
+                        if (textBoxInputHur.Text.ToUpper() == a.ToString())
+                        {
+                            urutanHur[i] = cekHur;
+                        }
+                        cekHur++;
+                    }
+                    cekHur = 0;
+                }
+            }
+            else
+            {
+                MessageBox.Show("Tidak ada huruf tersebut di kalimat");
+                textBoxInputHur.Clear();
+            }
+        }
+
+        private void textBoxInputHur2_TextChanged(object sender, EventArgs e)
+        {
+            if (textBoxInputKal.Text.ToUpper().Contains(textBoxInputHur.Text.ToUpper()))
+            {
+                for (int i = 0; i < 2; i++)
+                {
+                    for (char a = 'A'; a <= 'Z'; a++)
+                    {
+                        if (textBoxInputHur2.Text.ToUpper() == a.ToString())
+                        {
+                            urutanHur2[i] = cekHur2;
+                        }
+                        cekHur2++;
+                    }
+                    cekHur2 = 0;
+                }
+            }
+            else
+            {
+                MessageBox.Show("Tidak ada huruf tersebut di kalimat");
+                textBoxInputHur2.Clear();
+            }
+        }
+        public static int selisihUrutanMore = 0;
+        public static int selisihUrutanLess = 0;
+        public static int[] valueTiapHuruf = new int[100];
+        public static int[] valueOutput = new int[100];
+        public static string[] isiOutput = new string[100];
+        public static char[] inputArray = new char[100];
+        public static string output;
+       
+        private void buttonKonfirm_Click(object sender, EventArgs e)
+        {
+            inputArray = textBoxInputKal.Text.ToUpper().ToArray();
+            if (urutanHur[0] > urutanHur2[0])
+            {
+                selisihUrutanLess = urutanHur[0] - urutanHur2[0];
+            }
+            /*else if (urutanHur[1] > urutanHur2[1])
+            {
+                selisihUrutanLess = urutanHur[1] - urutanHur2[1];
+            }*/
+            else if (urutanHur2[0] > urutanHur[0])
+            {
+                selisihUrutanMore = urutanHur2[0] - urutanHur[0];
+            }
+            /*else if (urutanHur2[1] > urutanHur[1])
+            {
+                selisihUrutanMore = urutanHur2[1] - urutanHur[1];
+            }*/
+
+            for (int s = 0; s < textBoxInputKal.Text.Length; s++)
+            {
+                //cek tiap huruf di arrayInput, valuenya berapa
+                for (char a = 'A'; a <= 'Z'; a++)
+                {
+                    //jika huruf arrayInput ke s sama dengan a
+                    if (inputArray[s] == a)
+                    {
+                        valueTiapHuruf[s] = alpha.IndexOf(a);
+                    }
+                }
+            }
+            //mazz
+            //valueTiapHuruf: [1] = 13; [2] = 4; [3] = 25; [4] = 25 etc
+            //textboxinputhur = Z, urutanHur[0] = 25, urutanHur[1] = 51
+            //textBoxInpurHur2 = M, urutanHur2[0] = 13, urutanHur2[1] = 39
+            //urutanHur < urutanHur2, selisihUrutanMore = 13
+            //ubah value ke alphabet
+            //isiOutput[i], valueOutput[i]
+            for (int i = 0; i < textBoxInputKal.Text.Length; i++)
+            {
+                if (valueTiapHuruf[i] < selisihUrutanLess)
+                {
+                    valueOutput[i] = valueTiapHuruf[i] - selisihUrutanLess + 26;
+                }
+                else if (urutanHur[0] > urutanHur2[0])
+                {
+                    valueOutput[i] = valueTiapHuruf[i] - selisihUrutanLess;
+                }
+                else if (valueTiapHuruf[i] >= selisihUrutanMore)
+                {
+                    valueOutput[i] = valueTiapHuruf[i] + selisihUrutanMore - 26;
+                }
+                else if (urutanHur2[0] > urutanHur[0])
+                {
+                    valueOutput[i] = valueTiapHuruf[i] + selisihUrutanMore;
+                }
+            }
+
+            for (int i = 0; i < textBoxInputKal.Text.Length; i++)
+            {
+                for (char a = 'A'; a <= 'Z'; a++)
+                {
+                    if (alpha.IndexOf(a) == valueOutput[i])
+                    {
+                        isiOutput[i] = a.ToString();
+                    }
+                }
+                output += isiOutput[i];
+            }
+            labelOutput.Text = output;
+        }
+    }
+}
